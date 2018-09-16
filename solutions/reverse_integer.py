cc8495a1fc3e9a1554508a4504bd5816fcda9d1b
Shengfeng Li <cs.shengfeng.li@gmail.com>
2018-09-16 14:27:59 -0500

    Add a solustion for reverse integer

---
+++ solutions/reverse_integer.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        print('x: ' +str(x))
+        s = (x > 0) - (x < 0)
+        print('s: ' + str(s))
+        print('x*s: ' + str(x*s))
+        print('[::-1]: ' + (str(x*s)[::-1]))
+        r = int(str(x*s)[::-1])
+        return s*r * (r < 2**31)
