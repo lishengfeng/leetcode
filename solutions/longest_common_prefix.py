e660bc2d4aedc6ea03703d24e15488ad641d46e8
Shengfeng Li <cs.shengfeng.li@gmail.com>
2018-09-16 14:31:11 -0500

    Add a solution for longest common prefix

---
+++ solutions/longest_common_prefix.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ''
+        strs.sort(key = lambda x:len(x))
+        if not strs[0]:
+            return ''
+        if len(strs) == 1:
+            return strs[0]
+        for i in range(1,len(strs[0])+1):
+            if len(strs[0]) == 0:
+                return ''
+            s =strs[0][0:i]
+            for j in range(1,len(strs)):
+                if s!=strs[j][0:i]:return strs[0][0:i-1]
+        return strs[0]
